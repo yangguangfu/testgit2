8532d1d6a996a59f790d2b2ff3b00fb91984d7ba
atguiguafu <541433511@qq.com>
2016-05-17 12:32:28 +0800

    添加一键交友功能了

--- Hello.java
+++ Hello.java
@@ -3,6 +3,7 @@ public class Hello
 	public static void main(String arg[]){
 
 		System.out.pritnln("hello");
-	        System.out.pritnln("hello2");
+	    System.out.pritnln("hello2");
+		System.out.pritnln("Ò»¼ü½»ÓÑ");
 	}
 }
